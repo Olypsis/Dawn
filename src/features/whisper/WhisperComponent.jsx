0ff72c2924c64c13a88fe139db0fbaf209d46197
Edmund Miller <edmund.a.miller@gmail.com>
2019-01-22 14:39:49 -0600

    Add logic to log in with private key

--- src/features/whisper/WhisperComponent.jsx
+++ src/features/whisper/WhisperComponent.jsx
@@ -1,11 +1,33 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
+import queryString from 'query-string';
+import isEmpty from '../../util/is-empty';
 
 class Whisper extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pKey: '',
+    };
+  }
 
   async componentDidMount() {
-    await this.props.connectStatus();
-    await this.props.createStatusListener();
+    const params = new URLSearchParams(this.props.location.search);
+    const pKey = params.get('pKey');
+
+    const query = {
+      pKey,
+    };
+
+    console.log('QUERY', query);
+
+    if (!isEmpty(pKey)) {
+      // TODO
+      await this.props.connectStatus(pKey);
+    } else {
+      await this.props.connectStatus();
+      await this.props.createStatusListener();
+    }
   }
 
   render = () => <Fragment />;
